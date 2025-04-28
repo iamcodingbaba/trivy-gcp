6896552ec54884199d09a6505e6da89a350c68d9
iamcodingbaba <sahil.chhikara0727@gmail.com>
2025-04-28 13:57:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,6 @@ def home():
     return jsonify({"message": "Backend is running successfully!"})
 @app.route('/health')
 def health():
+    return jsonify({"status": "healthy"}), 200
+if __name__ == '__main__'
+   app.run(host='0.0.0.0', port=8000)
